7c7f884ff25cb6ca8db99c26298fad19a067e9d5
kim kyugnwon <gwon080870@gmail.com>
2024-01-04 11:55:41 +0900

    CHORE: Add get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:note_application/pages/main_page/main_page.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       home: MainPage(),
     );
   }
